95f7d96fb327f7e22b047075727e805c243a7e19
Adivya Jain <adivyajain2003@gmail.com>
2023-07-24 01:43:18 +0530

    Detail component in exerciseDetails page

--- src/Pages/ExerciseDetails.js
+++ src/Pages/ExerciseDetails.js
@@ -1,10 +1,42 @@
 import React, {useEffect,useState} from 'react'
-import { Pagination } from '@mui/material/Pagination'
+import { useParams } from 'react-router-dom'
+// import { Pagination } from '@mui/material/Pagination' 
+import { Box } from '@mui/material'
+import { exerciseOptions,fetchData } from '../utils/fetchData'
+import Detail from '../Components/Detail'
+import ExerciseVideos from '../Components/ExerciseVideos'
+import SimilarExercises from '../Components/SimilarExercises'
+
+const ExerciseDetail=()=>{
+  const [exerciseDetail,setExerciseDetail]=useState({});
+  const {id}=useParams();
+
+  useEffect(()=>{
+    const fetchExercisesData = async () =>{
+      const exerciseDbUrl ='https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData =await fetchData(`${exerciseDbUrl}/exercises/exercises/${id}`,exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+    }
+  })
+}
+
+
+
+
+
+
+
+
 const ExerciseDetails = () => {
   return (
-    <div>
-      yoyo
-    </div>
+    <Box>
+      <Detail ExerciseDetail={ExerciseDetail}/>
+      <ExerciseVideos/>
+      <SimilarExercises/>
+      
+    </Box>
   )
 }
 
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': '16d9ddaademsh9b66f96a5d4e806p1aa8c1jsn2abe0156d95b'
+    // 'X-RapidAPI-Key': '16d9ddaademsh9b66f96a5d4e806p1aa8c1jsn2abe0156d95b'
   },
 };
 
